d82de7701ae8322fac589d6da3866ba10b0fb7eb
Nicolò Giuliani <info@ngiuliani.com>
2021-01-06 19:00:21 +0100

    Interface construction and errors correction.

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 A new Flutter application. Codelab exercise.
 
 - Exercise: https://codelabs.developers.google.com/codelabs/flutter-firebase#1
+- Exercise: https://youtu.be/DqJ_KjFzL9I
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() {
   runApp(App());
 }
+
+final dummySnapshot = [
+  {"name": "Nome 1", "votes": 10},
+  {"name": "Nome 2", "votes": 10},
+  {"name": "Nome 3", "votes": 10},
+  {"name": "Nome 4", "votes": 10},
+  {"name": "Nome 5", "votes": 10},
+];
 
 class App extends StatelessWidget {
   @override
@@ -11,16 +20,78 @@ class App extends StatelessWidget {
       title: 'Firebase',
       debugShowCheckedModeBanner: true,
       theme: ThemeData(
-        primarySwatch: Colors.white,
+        primaryColor: Colors.white,
       ),
       home: HomePage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Firebase"),
+      ),
+      body: _buildBody(context),
+    );
   }
+
+  Widget _buildBody(BuildContext context) {
+    // TODO: Qui andrà ottenuta l'istanza effettiva da Cloud Firestore.
+    return _buildList(context, dummySnapshot);
+  }
+
+  Widget _buildList(BuildContext context, List<Map> snapshot) {
+    return ListView(
+      padding: const EdgeInsets.only(top: 16),
+      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+    );
+  }
+
+  Widget _buildListItem(BuildContext context, Map data) {
+    final record = Record.fromMap(data);
+
+    return Padding(
+      key: ValueKey(record.name),
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.grey,
+          ),
+          borderRadius: BorderRadius.circular(5),
+        ),
+        child: ListTile(
+          title: Text(record.name),
+          trailing: Text(record.votes.toString()),
+          onTap: () => print(record),
+        ),
+      ),
+    );
+  }
+}
+
+class Record {
+  final String name;
+  final int votes;
+  final DocumentReference reference;
+
+  Record.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map['name'] != null),
+        assert(map['votes'] != null),
+        name = map['name'],
+        votes = map['votes'];
+
+  Record.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data(), reference: snapshot.reference);
+
+  @override
+  String toString() => "Record<$name:$votes>";
 }
